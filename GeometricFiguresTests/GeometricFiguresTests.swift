ca3cdb1b90c745f520a402db7b40243b873b507a
Kyle Xiang <cxiang@lcs.on.ca>
2024-10-03 08:59:29 -0400

    Initial Commit

---
+++ GeometricFiguresTests/GeometricFiguresTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresTests.swift
+//  GeometricFiguresTests
+//
+//  Created by Chujun Xiang on 2024-10-03.
+//
+
+import Testing
+@testable import GeometricFigures
+
+struct GeometricFiguresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
